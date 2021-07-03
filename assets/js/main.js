b02bdeb6ba380ee37dd6b0a360836459a5d4c700
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-07-03 20:46:42 +0900

    2021.07.03 week1 - reaadme

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/**
+* Template Name: Eterna - v2.2.1
+* Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+window.onload = function() {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el:  ".swiper-pagination"
+    },
+    loop: true
+  });
+}
